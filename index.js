0836e870712da0da6f840a81a1d51f63aa746781
ju-zp <jhuntington@tutanota.com>
2018-11-19 14:49:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+function findMatching(array, name){
+    return array.filter(function(element){ return element.toLowerCase() == name.toLowerCase()});
+}
+
+function fuzzyMatch(array, str){
+    return array.filter(function(element){ return element.slice(0,str.length) == str});
+}
+
+function matchName(array, name){
+    return array.filter(function(element){return element["name"]==name});
+}
