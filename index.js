25ac7b3cc9b299ae823e3856f8e092a0933fc941
lola <slonanuga@gmail.com>
2018-11-19 13:03:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(arr, str) {
+
+    let arr2 = arr.filter(word => word.toLowerCase() == str.toLowerCase())
+    return arr2
+}
+
+function fuzzyMatch(arr, str) {
+    
+    let arr3 = arr.filter(word => word.startsWith(str))
+    return arr3
+
+}
+
+function matchName(obj, str) {
+    let arr4 = obj.filter(driver => driver.name == str)
+    return arr4
+
+
+}
